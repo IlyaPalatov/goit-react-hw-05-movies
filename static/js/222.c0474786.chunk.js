3390cd3f907b82be8439e456dc88d9daea5f7f1d
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2023-08-05 19:16:00 +0000

    Deploying to gh-pages from @ IlyaPalatov/goit-react-hw-05-movies@0d3c999c40ea829fb4af9300399a327e07987072 🚀

---
+++ static/js/222.c0474786.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[222],{222:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(e){var t=e.cast;return(0,a.jsx)("div",{children:t.map((function(e){return(0,a.jsxs)("div",{children:[(0,a.jsx)("img",{src:"https://image.tmdb.org/t/p/w200/".concat(e.profile_path),alt:e.name}),(0,a.jsx)("p",{children:e.name})]},e.id)}))})}}}]);
+//# sourceMappingURL=222.c0474786.chunk.js.map
